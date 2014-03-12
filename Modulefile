6604937b1c981bbe7ad196cad85309bd4ef1aea2
Sergio Oliveira <sergio@tracy.com.br>
2014-03-12 10:44:30 -0300

    Using stdlib ensure_packages

--- Modulefile
+++ Modulefile
@@ -5,3 +5,5 @@ author 'Tracy Web Technologies'
 summary 'Install pip using get-pip.py script'
 description 'Downloads get-pip.py and run it in order to install pip.'
 project_page 'https://github.com/TracyWebTech/puppet-pip'
+
+dependency 'puppetlabs/stdlib', '>= 3.2.1'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,13 @@
 
 class pip {
-  if ! defined(Package['curl']) {
-    package { 'curl': }
-  }
+  ensure_packages(['curl'])
 
   $get_pip_url = 'https://raw.github.com/pypa/pip/1.5.4/contrib/get-pip.py'
 
   exec { 'install-pip':
     command => "curl $get_pip_url | python",
-    path    => ['/usr/bin', '/usr/local/bin'],
+    path    => ['/usr/bin', '/usr/local/bin', '/bin/'],
     unless  => 'which pip',
+    require => Package['curl'],
   }
 }
