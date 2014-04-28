32039a10b24dc0ef85cd2bc65b90ef1f61f76060
Luan Pablo <luanpab@gmail.com>
2014-04-28 12:44:54 -0300

    Adding follow redirect flag to curl cmd

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class pip {
   $get_pip_url = 'https://raw.github.com/pypa/pip/1.5.4/contrib/get-pip.py'
 
   exec { 'install-pip':
-    command => "curl $get_pip_url | python",
+    command => "curl -L $get_pip_url | python",
     path    => ['/usr/bin', '/usr/local/bin', '/bin/'],
     unless  => 'which pip',
     require => Package['curl'],
