8f4b10cc88d6b2c9e52f005d3c9af6ffa6b5f20d
Sergio Oliveira <sergio@tracy.com.br>
2014-07-29 12:38:41 -0300

    Updating pip version

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 class pip {
   ensure_packages(['curl'])
 
-  $get_pip_url = 'https://raw.github.com/pypa/pip/1.5.4/contrib/get-pip.py'
+  $get_pip_url = 'https://raw.githubusercontent.com/pypa/pip/1.5.6/contrib/get-pip.py'
 
   exec { 'install-pip':
     command => "curl -L $get_pip_url | python",
