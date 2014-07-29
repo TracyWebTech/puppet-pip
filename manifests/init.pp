eb350ea654b977e3255e1976387362c267e7f961
Sergio Oliveira <sergio@tracy.com.br>
2014-07-29 19:32:00 -0300

    Pip installation for multiple python versions

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,4 @@
 
 class pip {
-  ensure_packages(['curl'])
-
-  $get_pip_url = 'https://raw.githubusercontent.com/pypa/pip/1.5.6/contrib/get-pip.py'
-
-  exec { 'install-pip':
-    command => "curl -L $get_pip_url | python",
-    path    => ['/usr/bin', '/usr/local/bin', '/bin/'],
-    unless  => 'which pip',
-    require => Package['curl'],
-  }
+  ensure_resource('pip::install', ['2.6', '2.7', '3.3', '3.4'])
 }
